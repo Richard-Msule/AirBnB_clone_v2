6ab4eead6b80dca964b8f0cb27115275b2db0e0c
Richard Msule <msuler@yahoo.com>
2023-04-06 13:16:09 -0700

    added solutions

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.25.194.222', '54.210.136.226']
 
 
 def do_clean(number=0):
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.25.194.222', '54.210.136.226']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
